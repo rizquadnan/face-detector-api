8c9157ae0ccee3e19c9691a5114254bdb2e4361a
adnan <rizquadnan@gmail.com>
2022-02-01 17:54:18 +0700

    format prettier

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
-const PORT = 3001;
-const API_BASE_URL = "/api/v1/";
+const PORT = 3001
+const API_BASE_URL = '/api/v1/'
 
-const createUser = ({ id = uuidv4(), name, email, password, uploadEntries = 0, joinDate = new Date().toDateString() }) => ({
-  id, 
+const createUser = ({
+  id = uuidv4(),
+  name,
+  email,
+  password,
+  uploadEntries = 0,
+  joinDate = new Date().toDateString(),
+}) => ({
+  id,
   name,
   email,
   password,
@@ -46,57 +53,63 @@ app.post(`${API_BASE_URL}sign-in`, (req, res) => {
   if (user && user.password === password) {
     res.send(createResponse({ status: 'SUCCESS', data: user }))
   } else {
+    res.status(404).send(
+      createResponse({
+        status: 'FAILED',
+        description: 'Email or password incorrect',
+      }),
+    )
+  }
+})
+
+app.post(`${API_BASE_URL}register`, (req, res) => {
+  const { name, email, password } = req.body
+
+  if (name && email && password) {
+    const user = createUser({ name, email, password })
+    users.push(user)
+
+    res.send(createResponse({ status: 'SUCCESS', data: user }))
+  } else {
     res
       .status(404)
       .send(
-        createResponse({
-          status: 'FAILED',
-          description: 'Email or password incorrect',
-        }),
-      ) 
+        createResponse({ status: 'FAILED', description: 'Input not valid' }),
+      )
   }
-});
-
-app.post(`${API_BASE_URL}register`, (req, res) => {
-  const { name, email, password } = req.body;
-
-  if (name && email && password) {
-    const user = createUser({ name, email, password });
-    users.push(user);
-
-    res.send(createResponse({ status: "SUCCESS", data: user }))
-  } else {
-    res.status(404).send(createResponse({ status: "FAILED", description: "Input not valid" }))
-  }
-});
+})
 
 app.get(`${API_BASE_URL}user/:id`, (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
 
-  const user = users.find(user => user.id === id);
+  const user = users.find((user) => user.id === id)
   if (user) {
-    res.send(createResponse({ status: "SUCCESS", data: user}))
+    res.send(createResponse({ status: 'SUCCESS', data: user }))
   } else {
-    res.status(404).send(createResponse({ status: "FAILED", description: "User not found" }))
+    res
+      .status(404)
+      .send(createResponse({ status: 'FAILED', description: 'User not found' }))
   }
-});
+})
 
 app.put(`${API_BASE_URL}user/:id`, (req, res) => {
-  const { id } = req.params;
-  const changedUser = req.body;
+  const { id } = req.params
+  const changedUser = req.body
 
-  const userIndex = users.findIndex(user => user.id === id);
+  const userIndex = users.findIndex((user) => user.id === id)
 
   if (userIndex !== -1) {
     users[userIndex] = {
       ...changedUser,
-      id
-    };
+      id,
+    }
 
-    res.send(createResponse({ status: "SUCCESS", data: changedUser}))
+    res.send(createResponse({ status: 'SUCCESS', data: changedUser }))
   } else {
-    res.status(404).send(createResponse({ status: "FAILED", description: "User not found" }))
+    res
+      .status(404)
+      .send(createResponse({ status: 'FAILED', description: 'User not found' }))
   }
-});
+})
 
 app.listen(PORT, () => console.log(`> Listening on port ${PORT}`))
